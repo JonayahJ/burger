25f44213dc0f284918242457d0ebe54605a9fdb9
Jonayah <jonayah@thinkhalcyon.com>
2020-08-09 15:03:09 -0400

    added the db files and connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burgers_name, devoured) VALUES ("Bleu Cheese Deluxe", true);
+INSERT INTO burgers (burgers_name, devoured) VALUES ("Bacon Cheeseburger with Carmelized Onions", false);
+INSERT INTO burgers (burgers_name, devoured) VALUES ("Impossilbe Burger", true);

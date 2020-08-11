d005bd84ad77559180983c69526428840ff5cde1
Jonayah <jonayah@thinkhalcyon.com>
2020-08-11 09:42:53 -0400

    swapping out var for let

--- config/connection.js
+++ config/connection.js
@@ -2,12 +2,18 @@
 require("dotenv").config()
 const mysql = require("mysql");
 
-const connection; 
+let connection; 
 if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 
 }else{
-  connection = mysql.createConnection(process.env.DB_URL);
+  connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: process.env.PW,
+  database: "burgers_db"
+});
 }
 
 // Make connection.
